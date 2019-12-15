12fa6d328ca11f8197af0b6404fdc8b4e73c77bb
aircjm <aircjm@gmail.com>
2019-12-15 23:10:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# mingran
+
+
+## 介绍
+基于 https://gitee.com/y_project/RuoYi-Vue 开发
+
+想要开发一个清单管理系统
+
+没有时间一步一步的搭建环境，直接拿现成的用了。后面自己优化吧
+反正只要自己足够菜，就有的学了
+
+## 使用
+
+### 初始化
+
+```bash
+docker run -it --name mysql -p 3306:3306 -v /d/dockerDrive/mysql:/app -e MYSQL_DATABASE=mingran -e MYSQL_USER=chenran -e MYSQL_PASSWORD=chenran123456 -e MYSQL_ROOT_PASSWORD=111111 wangxian/alpine-mysql
+```
+
+```bash
+docker run --name mingran-redis  -p 6379:6379 -d redis:5.0.7-alpine
+```
+
+
+
+### 代码自动生成
+自动生成代码其实蛮有意思的，后面研究下，减少crud的工作量其实蛮好的
+
+
+
---
+++ mingran/src/main/java/com/mingran/RuoYiApplication.java
@@ -0,0 +1,17 @@
+package com.mingran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+    public static void main(String[] args) {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("-----------------------------------项目启动成功----------------------------------");
+    }
+}
