acacc93782e092973b956ecab8a66ca9ccfb9ba4
chenran <aircjm@gmail.com>
2020-02-24 13:31:47 +0800

    add java doc

--- mingran-server/src/main/java/com/mingran/project/trello/service/BoardService.java
+++ mingran-server/src/main/java/com/mingran/project/trello/service/BoardService.java
@@ -12,9 +12,21 @@ import java.util.List;
  */
 public interface BoardService {
 
+    /**
+     * 获取board集合
+     *
+     * @param request 请求参数
+     * @return boardList
+     */
     List<Board> getBoardList(GetBoardRequest request);
 
 
+    /**
+     * 获取单个board
+     *
+     * @param request 请求参数
+     * @return Board
+     */
     Board getBoardDetail(GetBoardRequest request);
 
 }
